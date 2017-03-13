6157005181470253659f4e801fad940446396de9
Shreya_Infurnia <shreya@infurnia.com>
2017-03-13 14:37:59 +0530

    login functionality working

--- src/login.js
+++ src/login.js
@@ -1,3 +1,22 @@
 /// <reference path="../js/angular.js" />
 
 var app = angular.module("myModule",[]);
+var login = function ($scope) {
+	var login_params = {
+		username : "",
+		password : ""
+	}
+	
+	$scope.login_params = login_params;
+	$scope.enter_login = function(login_params) {
+		if ((login_params.username == 'shreya' && login_params.password == '12345') || (login_params.username == 'lomadi' && login_params.password == '23456')) {
+			window.location = '../htmls/site.html';
+		}
+		else {
+			alert("Wrong Credentials");
+		}
+	}
+}
+
+app.controller("login", login);
+
---
+++ src/site.js
@@ -0,0 +1,3 @@
+/// <reference path="../js/angular.js" />
+
+var app = angular.module("module1",[]);
